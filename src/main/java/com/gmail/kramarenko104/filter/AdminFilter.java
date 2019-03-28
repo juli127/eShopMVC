62398eabba7cec8382236bd2ae9ede918bcd2e74
julia_kramarenko <kramarenko104@gmail.com>
2019-03-28 18:59:40 +0200

    configs + pom.xml update

--- src/main/java/com/gmail/kramarenko104/filter/AdminFilter.java
+++ src/main/java/com/gmail/kramarenko104/filter/AdminFilter.java
@@ -1,20 +1,11 @@
 package com.gmail.kramarenko104.filter;
 
-import org.apache.log4j.Logger;
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
+import java.io.IOException;
 
 public class AdminFilter implements Filter {
-
-    private static Logger logger = Logger.getLogger(AdminFilter.class);
 
     public AdminFilter() {
     }
