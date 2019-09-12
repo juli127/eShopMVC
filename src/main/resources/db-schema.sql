8db1a2ad91c37d954c94c2733ce697e2a2696443
Iuliia Kramarenko <juli127@bigmir.net>
2019-09-12 19:18:30 +0300

    Update db-schema.sql

--- src/main/resources/db-schema.sql
+++ src/main/resources/db-schema.sql
@@ -2,53 +2,53 @@ CREATE DATABASE IF NOT EXISTS eshopdb CHARACTER SET utf8 COLLATE utf8_general_ci
 USE eshopdb;
 
 CREATE TABLE IF NOT EXISTS users(
-  id INT AUTO_INCREMENT,
-  login VARCHAR(30) UNIQUE NOT NULL,
+  userId INT AUTO_INCREMENT,
+  login VARCHAR(100) UNIQUE NOT NULL,
   password VARCHAR(30) NOT NULL,
   name VARCHAR(50),
   address VARCHAR(50),
   comment VARCHAR(100),
-  PRIMARY KEY (id)
+  PRIMARY KEY (userId)
 );
 
 CREATE TABLE IF NOT EXISTS products (
-  id INT AUTO_INCREMENT,
+  productId INT AUTO_INCREMENT,
   name VARCHAR(100) UNIQUE NOT NULL,
   category INT,
   price INT,
   description VARCHAR(300),
   image VARCHAR(100),
-  PRIMARY KEY (id),
+  PRIMARY KEY (productId)
   ON DELETE CASCADE
   ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS carts (
-  id INT AUTO_INCREMENT,
-  userId INT,
-  productId INT,
-  quantity INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (userId) REFERENCES users_test(id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE,
-  FOREIGN KEY (productId) REFERENCES products_test(id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS orders_test (
-  id INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS orders (
+  orderId INT AUTO_INCREMENT,
   orderNumber INT,
   status VARCHAR(100),
   userId INT,
   productId INT,
   quantity INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (userId) REFERENCES users_test(id)
+  PRIMARY KEY (orderId),
+  FOREIGN KEY (userId) REFERENCES users(userId)
   ON DELETE CASCADE
   ON UPDATE CASCADE,
-  FOREIGN KEY (productId) REFERENCES products_test(id)
+  FOREIGN KEY (productId) REFERENCES products(productId)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS carts (
+  cartId INT AUTO_INCREMENT,
+  userId INT,
+  productId INT,
+  quantity INT,
+  PRIMARY KEY (cartId),
+  FOREIGN KEY (userId) REFERENCES users(userId)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+  FOREIGN KEY (productId) REFERENCES products(productId)
   ON DELETE CASCADE
   ON UPDATE CASCADE
 );
